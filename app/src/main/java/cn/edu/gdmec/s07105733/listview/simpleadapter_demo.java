2ec621faf58c6bf1af55dad116517efa7ec7a727
gdmen07150733 <413359404>
2016-11-06 20:36:35 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07105733/listview/simpleadapter_demo.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.s07105733.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class simpleadapter_demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simpleadapter_demo);
+    }
+}
